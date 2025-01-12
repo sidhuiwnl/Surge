211faa0c0f4846e5145de71903623c46fadaa2bd
Sidharth <sidharthinfernal@gmail.com>
2025-01-12 22:23:26 +0530

    modify package.json

--- spawner/render-build.sh
+++ spawner/render-build.sh
@@ -1,26 +1,32 @@
 #!/usr/bin/env bash
 #exit on error
-
 set -o errexit
-#Install dependencies
 
+# Install dependencies
 npm install
-#Uncomment this line if you need to build your project
+
+# Build project
 npm run build
-#Ensure the Puppeteer cache directory exists
 
+# Create all necessary directories
+RENDER_CACHE_DIR=/opt/render/project/src/.cache/puppeteer/chrome
 PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+
+mkdir -p $RENDER_CACHE_DIR
 mkdir -p $PUPPETEER_CACHE_DIR
-#Install Puppeteer and download Chrome
 
+# Install Puppeteer and download Chrome
 npx puppeteer browsers install chrome
-#Store/pull Puppeteer cache with build cache
 
+# Store/pull Puppeteer cache with build cache
 if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
-echo "...Copying Puppeteer Cache from Build Cache"
-# Copying from the actual path where Puppeteer stores its Chrome binary
-cp -R /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
+    echo "...Copying Puppeteer Cache from Build Cache"
+    # Create parent directories if they don't exist
+    mkdir -p $(dirname $PUPPETEER_CACHE_DIR)
+    cp -R $RENDER_CACHE_DIR $PUPPETEER_CACHE_DIR
 else
-echo "...Storing Puppeteer Cache in Build Cache"
-cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
+    echo "...Storing Puppeteer Cache in Build Cache"
+    # Create parent directories if they don't exist
+    mkdir -p $(dirname $RENDER_CACHE_DIR)
+    cp -R $PUPPETEER_CACHE_DIR $RENDER_CACHE_DIR
 fi
